3a5b4debc8a0c4813a8bc6ce7c6aca10c8e65ebb
JammingEnd <57032230+JammingEnd@users.noreply.github.com>
2021-06-30 13:07:01 +0100

    added  movement

---
+++ roguelike FPS with jons/Assets/scripts/PlayerMovementController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float raycastDistance;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    /// <summary>
+    /// move in all 2d axis using unity build-in move input (wasd movement) and multiplies that with speed and a fixed update
+    /// </summary>
+    private void Move()
+    {
+        float hAxis = Input.GetAxisRaw("Horizontal");
+        float vAxis = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(hAxis, 0, vAxis) * speed * Time.fixedDeltaTime;
+
+        Vector3 newPosition = rb.position + rb.transform.TransformDirection(movement);
+        rb.MovePosition(newPosition);
+    }
+    
+
+    
+}
